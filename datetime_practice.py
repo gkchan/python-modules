a893bab5a4ca371d347f51a89f94a159b7a0d2dc
Grace <gkchan09@gmail.com>
2018-02-04 03:16:29 +0000

    Add time from datetime

--- datetime_practice.py
+++ datetime_practice.py
@@ -1,7 +1,13 @@
-from datetime import datetime, date
+from datetime import datetime, date, time
 
 print datetime.now()
 print datetime.today()
+print datetime.today().year
+print datetime.now().weekday()
+
 
 print date.today()
 print date.today().year
+
+
+print time.hour
