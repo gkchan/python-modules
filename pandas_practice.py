4118d35150a3fc27146c41c1bac62b93fa14cf16
Grace <gkchan09@gmail.com>
2018-02-06 01:59:17 +0000

    Add data to dataframe

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,4 +1,4 @@
 import pandas as pd 
 
-dataframe1 = pd.DataFrame(columns=["Column1", "Column2"])
+dataframe1 = pd.DataFrame(data=[("data1", "data2")], columns=["Column1", "Column2"])
 print dataframe1
