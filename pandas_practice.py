3c40e31958e703fc32e6c65729db63ce9dae1c8b
Grace <gkchan09@gmail.com>
2018-02-07 05:27:56 +0000

    Add another row to dataframe

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,4 +1,4 @@
 import pandas as pd 
 
-dataframe1 = pd.DataFrame(data=[("data1", "data2")], columns=["Column1", "Column2"])
+dataframe1 = pd.DataFrame(data=[("data1", "data2"), ("data3", "data4")], columns=["Column1", "Column2"])
 print dataframe1
