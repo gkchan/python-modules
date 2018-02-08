45617e8d50e1778566127b2e7c5ccfbd460ebe12
Grace <gkchan09@gmail.com>
2018-02-08 05:45:36 +0000

    Add pivot table

--- pandas_practice.py
+++ pandas_practice.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 dataframe1 = pd.DataFrame(data=[("data1", "data2"), ("data3", "data4")], columns=["Column1", "Column2"])
 print dataframe1
+
+dataframe2 = pd.DataFrame(data=[("red", 2, "dog"), ("blue", 5, "cat")], columns=["Color", "Number", "Animal"])
+print dataframe2
+
+pivot_table = pd.pivot_table(dataframe2, values="Number", columns="Color", index="Animal")
+print pivot_table
