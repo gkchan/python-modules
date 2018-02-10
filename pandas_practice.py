ee188dc8e857c9cf5bfb6fd3ea2ba3aa89408ce8
Grace <gkchan09@gmail.com>
2018-02-10 04:37:13 +0000

    Group by color

--- pandas_practice.py
+++ pandas_practice.py
@@ -3,10 +3,12 @@ import pandas as pd
 dataframe1 = pd.DataFrame(data=[("data1", "data2"), ("data3", "data4")], columns=["Column1", "Column2"])
 print dataframe1
 
-dataframe2 = pd.DataFrame(data=[("red", 2, "dog"), ("blue", 5, "cat")], columns=["Color", "Number", "Animal"])
+dataframe2 = pd.DataFrame(data=[("red", 2, "dog"), ("blue", 5, "cat"), ("red", 7, "fish")], columns=["Color", "Number", "Animal"])
 print dataframe2
 
 pivot_table = pd.pivot_table(dataframe2, values="Number", columns="Color", index="Animal")
 print pivot_table
 
 print dataframe2["Number"].sum()
+
+print dataframe2.groupby("Color").groups
